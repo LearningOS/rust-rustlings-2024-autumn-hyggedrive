a5a75c348844b9ced06cf7533f6396c223631ad8
Linux <123@163.com>
2024-10-06 19:12:06 -0700

    solve enums1

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -4,14 +4,19 @@
 
 // I AM NOT DONE
 
+use std::string;
+
 #[derive(Debug)]
 enum Message {
-    // TODO: define a few types of messages as used below
+    Quit,
+    Echo(String),
+    Move { x : i32, y : i32 },
+    ChangeColor(i32,i32,i32),
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo(String::from("hello,cargo!")));
+    println!("{:?}", Message::Move { x: 3, y: 4 });
+    println!("{:?}", Message::ChangeColor(255,165,0));
 }
