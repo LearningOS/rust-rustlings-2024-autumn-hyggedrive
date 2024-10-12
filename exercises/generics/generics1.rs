3529333d44d4b3aaaa9c71aa47184c5e1600526c
Linux <123@163.com>
2024-10-12 09:38:06 -0700

    solve generics1

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -8,7 +8,9 @@
 
 // I AM NOT DONE
 
+use std::string;
+
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
