1b03d4aed392044c2d7b8e607270b7c333f691f7
Linux <123@163.com>
2024-10-20 11:11:56 -0700

    solved

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::collections;
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,8 +15,10 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
-    }
+        Some(first_char) => {
+            // 大写第一个字符并且加上剩余部分
+            first_char.to_uppercase().collect::<String>() + &c.as_str().to_string()
+        },}
 }
 
 // Step 2.
@@ -24,7 +26,10 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words
+        .iter()
+        .map(|word| capitalize_first(word))
+        .collect()
 }
 
 // Step 3.
@@ -32,7 +37,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words
+    .iter()
+    .map(|word| capitalize_first(word))
+    .collect::<Vec<String>>()
+    .join(" ")
 }
 
 #[cfg(test)]
@@ -58,6 +67,6 @@ mod tests {
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        assert_eq!(capitalize_words_string(&words), "Hello World");
+        assert_eq!(capitalize_words_string(&words), "Hello   World");
     }
 }
